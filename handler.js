b4b398eeea265418b2cc1b68b99ccc6c8b3c7877
Anderson Carvalho <anderson.carvalho@talkdesk.com>
2019-04-23 15:34:44 +0100

    Give preference to environment variables when running local

--- handler.js
+++ handler.js
@@ -1,14 +1,17 @@
 'use strict';
 
+require('dotenv').config()
+
 const createCloudantConnection = require('./cloudant').createCloudantConnection
 
 function hello(params) {
   return new Promise((resolve, reject) => {
     const envParameters = params['0']
-    console.log('envParameters.CLOUDANT_DB', envParameters.CLOUDANT_DB)
-    console.log('envParameters.CLOUDANT_URL', envParameters.CLOUDANT_URL)
-    const cloudant = createCloudantConnection(envParameters.CLOUDANT_URL, envParameters.CLOUDANT_API_KEY)
-    cloudant.db.use(envParameters.CLOUDANT_DB).insert(params.document).then((insertResult)=> {
+    const cloudantUrl = process.env.CLOUDANT_URL || envParameters.CLOUDANT_URL
+    const cloudantApiKey = process.env.CLOUDANT_API_KEY || envParameters.CLOUDANT_API_KEY
+    const cloudantDatabase = process.env.CLOUDANT_DB || envParameters.CLOUDANT_DB
+    const cloudant = createCloudantConnection(cloudantUrl, cloudantApiKey)
+    cloudant.db.use(cloudantDatabase).insert(params.document).then((insertResult)=> {
       resolve(insertResult)
     }).catch((err)=>{
       reject(err)
